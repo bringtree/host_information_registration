d2b0e4a4d386a9a22e9c04619b22be18fc83e8de
bringtree <bringtree@qq.com>
2017-05-13 03:04:57 +0800

    完成管理员页面

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -1,89 +1,117 @@
 <template>
   <div>
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-      <el-table-column
-        label="id"
-        sortable
-        width="180">
-        <template scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="用户名"
-      >
-        <template scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.username }}</span>
-        </template>
-      </el-table-column>
+    <el-button type="text" @click="registerFormVisible = true">点击添加新用户</el-button>
+    <div>
+      <el-dialog title="添加账号" v-model="registerFormVisible">
+        <el-form :model="registerForm">
+          <el-form-item label="用户名" :label-width="modifyLabelWidth">
+            <el-input v-model="registerForm.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="modifyLabelWidth">
+            <el-input v-model="registerForm.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" :label-width="modifyLabelWidth">
+            <el-input v-model="registerForm.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="状态" :label-width="modifyLabelWidth">
+            <el-select v-model="registerForm.status" placeholder="请选择状态">
+              <el-option label="未启用" value=0></el-option>
+              <el-option label="启用" value=1></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="registerFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="registerSubmit">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
 
-      <el-table-column
-        label="用户密码"
-      >
-        <template scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.password }}</span>
-        </template>
-      </el-table-column>
+    <div>
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          label="id"
+          sortable
+          width="180">
+          <template scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="用户名"
+        >
+          <template scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.username }}</span>
+          </template>
+        </el-table-column>
 
-      <el-table-column
-        label="状态"
-      >
-        <template scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.status==1?'启用':'未启用' }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="最后修改日期"
-        sortable
-        width="180">
-        <template scope="scope">
-          <el-icon name="time"></el-icon>
-          <span style="margin-left: 10px">{{ scope.row.updateTime }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作"
-                       width="200"
-      >
-        <template scope="scope">
-          <el-button
-            size="small"
-            @click="handleEdit(scope.$index, scope.row)">编辑
-          </el-button>
-          <el-button
-            size="small"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-          >删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column
+          label="用户密码"
+        >
+          <template scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.password }}</span>
+          </template>
+        </el-table-column>
 
-    <!-- Form -->
-    <el-dialog title="修改信息" v-model="modifyFormVisible">
-      <el-form :model="modifyForm">
-        <el-form-item label="用户名" :label-width="modifyLabelWidth">
-          <el-input v-model="modifyForm.username" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" :label-width="modifyLabelWidth">
-          <el-input v-model="modifyForm.password" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="状态" :label-width="modifyLabelWidth">
-          <el-select v-model="modifyForm.status" placeholder="请选择状态">
-            <el-option label="未启用" value=0></el-option>
-            <el-option label="启用" value=1></el-option>
-          </el-select>
-        </el-form-item>
+        <el-table-column
+          label="状态"
+        >
+          <template scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.status==1?'启用':'未启用' }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="最后修改日期"
+          sortable
+          width="180">
+          <template scope="scope">
+            <el-icon name="time"></el-icon>
+            <span style="margin-left: 10px">{{ scope.row.updateTime }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作"
+                         width="200"
+        >
+          <template scope="scope">
+            <el-button
+              size="small"
+              @click="handleEdit(scope.$index, scope.row)">编辑
+            </el-button>
+            <el-button
+              size="small"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- Form -->
+      <el-dialog title="修改信息" v-model="modifyFormVisible">
+        <el-form :model="modifyForm">
+          <el-form-item label="用户名" :label-width="modifyLabelWidth">
+            <el-input v-model="modifyForm.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="modifyLabelWidth">
+            <el-input v-model="modifyForm.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="状态" :label-width="modifyLabelWidth">
+            <el-select v-model="modifyForm.status" placeholder="请选择状态">
+              <el-option label="未启用" value=0></el-option>
+              <el-option label="启用" value=1></el-option>
+            </el-select>
+          </el-form-item>
 
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="modifyFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="modifySubmit">确 定</el-button>
-      </div>
-    </el-dialog>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="registerFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="registerSubmit">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
@@ -122,9 +150,15 @@
           }
         ],
         modifyFormVisible: false,
+        registerFormVisible: false,
         modifyForm: {
           index: '',
           id: '',
+          username: '',
+          password: '',
+          status: ''
+        },
+        registerForm: {
           username: '',
           password: '',
           status: ''
@@ -188,6 +222,27 @@
               type: 'success',
               message: '编辑成功!'
             })
+          })
+          .catch(() => {
+            this.$message({
+              type: 'error',
+              message: '网络故障'
+            })
+          })
+      },
+      registerSubmit () {
+        this.$ajax.post('', this.registerForm)
+          .then((res) => {
+            // 这一块 还差一个时间和id没更新
+            var o = {}
+            o.username = this.registerForm.username
+            o.password = this.registerForm.password
+            o.status = this.registerForm.status
+            this.tableData.push(o)
+            this.$message = {
+              type: 'success',
+              message: '注册成功'
+            }
           })
           .catch(() => {
             this.$message({
